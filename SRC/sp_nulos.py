b2967564a9e5873d0679cf08f9af845bc2a538bb
Vic-tor123 <victorcarballido@gmail.com>
2025-11-11 23:55:33 +0000

    Ajustes exclusion columnas ID

--- SRC/sp_nulos.py
+++ SRC/sp_nulos.py
@@ -11,10 +11,10 @@ import seaborn as sns
 def estadist_col_cat (df): 
     """
     Qué realiza la función  
-        Calcula y muestra la distribución porcentual de valores para todas las columnas categóricas del DataFrame.  
-      
+        Calcula y muestra la distribución porcentual de valores para todas las columnas categóricas del DataFrame. Excluyendo las columnas ID  
+        
     Qué incluye el análisis  
-        - Recorre todas las columnas de tipo texto (object).  
+        - Recorre todas las columnas de tipo texto (object) excluyendo las columnas ID  
         - Imprime el nombre de la columna en mayúsculas.  
         - Muestra el porcentaje de cada categoría respecto al total de registros.  
         - Separa cada columna con una línea divisoria para mayor claridad.  
@@ -25,7 +25,10 @@ def estadist_col_cat (df):
     Returns:
     None
     """
+    id_cols = ['order_id', 'customer_id', 'product_id', 'seller_id', 'review_id']
+    
     for col in df.select_dtypes(include='O').columns:
-        print (col.upper())
-        print (df[col].value_counts()/df.shape[0]*100)
-        print('------------------------')         
+        if col not in id_cols:
+            print (col.upper())
+            print (df[col].value_counts()/df.shape[0]*100)
+            print('------------------------')         
